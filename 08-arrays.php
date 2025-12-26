1309555b450e2aed80c5316b199c51b82fd165fd
Edward Martinez <emartinezj1802@gmail.com>
2025-12-26 14:58:18 -0500

    se incluyen arregos y asociative arrays

--- 08-arrays.php
+++ 08-arrays.php
@@ -1,6 +1,6 @@
 <?php include 'includes/header.php';
 
-$carrito = ['Tablet','Celular','Portatil','PC Gamer'];
+$carrito = ['Tablet','Celular','Portatil','PC Gamer']; //este estilo de arreglo se usa mas en Laravel
 $carrito2 = ['Tablet','Celular','Portatil','PC Gamer'];
 
 echo "1. Arrays:";
@@ -32,11 +32,31 @@ echo "</pre>";
 echo "<br>";
 echo "<br>";
 
-echo "5. agregar al inicio y al final un elemento en el arreglo";
+echo "5. agregar al inicio y al final un elemento en el arreglo: ";
+echo "<br>";
+echo "<br>";
+echo "* Al final: 'Audifonos' ver indice [5]";
+array_push($carrito2, 'Audifonos');
+echo "<pre>";
+var_dump($carrito2);
+echo "</pre>";
+echo "<br>";
+echo "<br>";
+echo "* Al inicio: 'Monitor curvo' ver indice [0]";
+array_unshift($carrito2, 'Monitor curvo');
+echo "<pre>";
+var_dump($carrito2);
+echo "</pre>";
+echo "<br>";
+echo "<br>";
 
-
-
-
+echo "6. Otra forma de crear arreglos de la forma: $ clientes = array('cliente 1', 'cliente 2', 'cliente 3')";
+$clientes = array('cliente 1', 'cliente 2', 'cliente 3'); // este estilo de arreglo se usa mas en Wordpress
+echo "<pre>";
+var_dump($clientes);
+echo "</pre>";
+echo "<br>";
+echo "<br>";
 
 
 include 'includes/footer.php';
--- 09-arrays-assoc.php
+++ 09-arrays-assoc.php
@@ -1,6 +1,64 @@
 <?php include 'includes/header.php';
 
+$cliente = [
+    "nombre" => 'juan',
+    "saldo" => 200,
+    "información" => [
+        "tipo" => 'Premium',
+        "disponible" => true,
+        "credito" => 100
+    ]
+];
 
+echo "1. ver el arreglo: ";
+echo "<br>";
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
+echo "<br>";
+echo "<br>";
+
+echo "2. acceder a un objeto dentro del arreglo: ";
+echo "<br>";
+var_dump($cliente ["nombre"]); //accedemos directamente por el nombre de la propiedad
+echo "</pre>";
+echo "<br>";
+echo "<br>";
+
+echo "3. imprimir directamente el valor del objeto dentro del array:  ";
+echo "<br>";
+echo "nombre: ";
+echo $cliente['nombre'];
+echo "<br>";
+echo "saldo: ";
+echo $cliente['saldo'];
+echo "<br>";
+echo "<br>";
+
+echo "4. acceder a la informacion de un arreglo dentro de otro arreglo:  ";
+echo "<br>";
+echo "Información tipo: ";
+echo $cliente['información']['tipo'];
+echo "<br>";
+echo "Información disponible: ";
+echo var_dump($cliente['información']['disponible']); //muestra el tip y valor del elemento en este caso que es un booleano y su valor es true
+echo "<br>";
+echo $cliente['información']['disponible']; //este solo muestra si es true  y si es false 0
+echo "<br>";
+echo "Saldo disponible: ";
+echo $cliente['información']['disponible'];
+echo "<br>";
+echo "<br>";
+
+echo "5. Agregar mas propiedades al objeto: ";
+echo "<pre>";
+$cliente["codigo"] = 1019021157;
+echo "Ver punto 1 ya debe aparecer credito asignado";
+echo "</pre>"; // al validar el var_dump de $cliente me lo muestra con 
+echo "Credito: ";
+echo $cliente['información']['credito'];
+echo "<br>";
+echo "<br>";
 
 
 include 'includes/footer.php';

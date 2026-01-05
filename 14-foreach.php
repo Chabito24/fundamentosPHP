e319f69742058166e93abb134747d816eaab1d69
Edward Martinez <emartinezj1802@gmail.com>
2026-01-05 12:06:13 -0500

    se agregan ejemplos de foreach, forma 1 con if y dforma 2 con ternario

--- 14-foreach.php
+++ 14-foreach.php
@@ -31,5 +31,53 @@ foreach ( $cliente as $key => $valor){ //Relacionamos la variable cliente as la 
 }
 echo "<br><br>";
 
+echo "4. Ejemplos de foreach: <br><br>";
+
+$productos = [
+    [
+        'nombre' => 'Mouse',
+        'precio' => 200,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Monitor Curvo',
+        'precio' => 300,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'MacBook Air4',
+        'precio' => 400,
+        'disponible' => false
+    ]
+];
+
+echo "Forma 1: con if: <br><br>";
+
+foreach( $productos as $producto) {?>
+    <li>
+        <p>Producto: <?php echo $producto['nombre']; ?> </p>
+        <p>Precio: <?php echo $producto['precio']; ?></p>
+        <?php
+            if ($producto['disponible']) {
+            echo "<p>Disponible</p>";
+        } else {
+            echo "<p>No disponible</p>";
+        }
+        ?>
+    </li>
+    <?php
+}
+echo "<br><br><br>";
+echo "Forma 2: con operador ternario: <br><br>";
+
+foreach( $productos as $producto) {?>
+    <li>
+        <p>Producto: <?php echo $producto['nombre']; ?> </p>
+        <p>Precio: <?php echo $producto['precio']; ?></p>
+        <p><?php echo ($producto['disponible']) ? 'Disponible' : 'No disponible';?></p>
+    </li>
+    <?php
+}
+
 
 include 'includes/footer.php';
